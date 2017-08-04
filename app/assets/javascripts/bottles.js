725b7f16480eea243078db088790a11c74ea24bf
Evgeny Matveev <evgeny@mak-design.me>
2017-08-04 13:43:31 +0300

    refactor buttonContainer

--- app/assets/javascripts/bottles.js
+++ app/assets/javascripts/bottles.js
@@ -1,8 +1,8 @@
 document.addEventListener('turbolinks:load', () => {
-  const burger = document.getElementById('burger')
-  const burgerCloseBtn = document.getElementById('burgerCloseBtn')
+  const headerOpenBtn = document.getElementById('headerOpenBtn')
+  const menuHeaderCloseBtn = document.getElementById('menuHeaderCloseBtn')
   const menu = document.getElementById('menu')
 
-  burger.onclick = () => menu.classList.remove('hidden')
-  burgerCloseBtn.onclick = () => menu.classList.add('hidden')
+  headerOpenBtn.onclick = () => menu.classList.remove('hidden')
+  menuHeaderCloseBtn.onclick = () => menu.classList.add('hidden')
 })
