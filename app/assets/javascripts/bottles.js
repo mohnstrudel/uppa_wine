f2df630d5b50f30a705c39fce67192ff220d75b9
Dmitry Nikulnikov <git@m0zg.net>
2017-08-08 16:35:43 +0300

    800px menu signature are beautiful again!

--- app/assets/javascripts/bottles.js
+++ app/assets/javascripts/bottles.js
@@ -1,8 +1,10 @@
 document.addEventListener('turbolinks:load', () => {
   const headerOpenBtn = document.getElementById('headerOpenBtn')
+  const menuHeaderOpenBtn = document.getElementById('menuHeaderOpenBtn')
   const menuHeaderCloseBtn = document.getElementById('menuHeaderCloseBtn')
   const menu = document.getElementById('menu')
 
-  headerOpenBtn.onclick = () => menu.classList.remove('hidden')
-  menuHeaderCloseBtn.onclick = () => menu.classList.add('hidden')
+  menuHeaderOpenBtn.onclick = () => menu.classList.remove('mobileHelper')
+  headerOpenBtn.onclick = () => menu.classList.remove('mobileHelper')
+  menuHeaderCloseBtn.onclick = () => menu.classList.add('mobileHelper')
 })
