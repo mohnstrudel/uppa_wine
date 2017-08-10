aeea6e135958e262e73f03e672ea2630cb3a9d6a
Evgeny Matveev <evgeny@mak-design.me>
2017-08-10 17:58:34 +0300

    add menu back animations

--- app/assets/javascripts/bottles.js
+++ app/assets/javascripts/bottles.js
@@ -3,8 +3,21 @@ document.addEventListener('turbolinks:load', () => {
   const menuHeaderOpenBtn = document.getElementById('menuHeaderOpenBtn')
   const menuHeaderCloseBtn = document.getElementById('menuHeaderCloseBtn')
   const menu = document.getElementById('menu')
+  const dim = document.getElementById('dim')
 
-  menuHeaderOpenBtn.onclick = () => menu.classList.remove('mobileHelper')
+  menuHeaderOpenBtn.onclick = () => {
+    menu.classList.remove('mobileHelper')
+    dim.style.display = 'block'
+    setTimeout(() => {
+      dim.style.opacity = '0.6'
+    }, 50)
+  }
   headerOpenBtn.onclick = () => menu.classList.remove('mobileHelper')
-  menuHeaderCloseBtn.onclick = () => menu.classList.add('mobileHelper')
+  menuHeaderCloseBtn.onclick = () => {
+    menu.classList.add('mobileHelper')
+    dim.style.opacity = '0'
+    setTimeout(() => {
+      dim.style.display = 'none'
+    }, 300)
+  }
 })
