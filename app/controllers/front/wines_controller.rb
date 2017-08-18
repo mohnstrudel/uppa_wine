e277d6973830128e3f178d99002fdb4aa8e68b30
Anton Kostin <schnibba@gmail.com>
2017-08-18 22:10:46 +0300

    Dynamic wines and wine page.

--- app/controllers/front/wines_controller.rb
+++ app/controllers/front/wines_controller.rb
@@ -1,8 +1,10 @@
 class Front::WinesController < FrontController
   def index
+    @wines = Wine.all
   end
 
   def show
+    @wine = Wine.find(params[:id])
   end
 
   def home
--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,2 +1,27 @@
 module FrontHelper
+
+  def get_bottle_class(index)
+    case index
+    when 0
+      return "firstBottle"
+    when 1
+      return "secondBottle"
+    when 2
+      return "thirdBottle"
+    when 3
+      return "fourthBottle"
+    when 4
+      return "fifthBottle"
+    when 5
+      return "sixthBottle"
+    end
+  end
+
+  def image_tag_or_default(object, field, options = {})
+    if object.send(field).present?
+      return image_tag(object.send(field).url, class: options[:class])
+    else
+      return image_tag "bottles/second.png", class: options[:class]
+    end
+  end
 end
