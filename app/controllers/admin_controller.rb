e83d89322678533637f5ca1ecf80aba7fef230dd
Anton Kostin <schnibba@gmail.com>
2017-12-04 19:58:08 +0300

    User -> Admin fix.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,6 +16,6 @@ class AdminController < ApplicationController
   end
 
     def verify_is_superadmin
-    (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.superadmin?)
+    (current_admin.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_admin.superadmin?)
   end
 end
